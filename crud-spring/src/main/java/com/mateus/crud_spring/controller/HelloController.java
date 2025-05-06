8103377e7dfde7eb00e32d9951fff38568775c31
Mateus H M Ferreira <mateushenrique123@live.com>
2025-05-06 14:39:00 -0300

    Hello World

---
+++ crud-spring/src/main/java/com/mateus/crud_spring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.mateus.crud_spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/")
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
