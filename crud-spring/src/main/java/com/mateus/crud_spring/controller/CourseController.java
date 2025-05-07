5400e737e299075f16fc9168dcf81bbe4d82f7c5
Mateus H M Ferreira <mateushenrique123@live.com>
2025-05-07 17:07:33 -0300

    Criação do endpoint para criar um curso (HTTP POST)

--- crud-spring/src/main/java/com/mateus/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/mateus/crud_spring/controller/CourseController.java
@@ -2,10 +2,9 @@ package com.mateus.crud_spring.controller;
 
 import com.mateus.crud_spring.model.Course;
 import com.mateus.crud_spring.repository.CourseRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,11 @@ public class CourseController {
     @GetMapping
     public List<Course> list(){
         return this.courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return this.courseRepository.save(course);
     }
 }
