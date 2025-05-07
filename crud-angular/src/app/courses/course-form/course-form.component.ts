33df0929920eda38955102ee82c6fa7e150917ed
Mateus H M Ferreira <mateushenrique123@live.com>
2025-05-07 16:34:33 -0300

    Slavando os dados com HTTP Post + tratamentos

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -9,7 +11,11 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar
+  ) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [ null]
@@ -20,10 +26,16 @@ export class CourseFormComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log("Submit")
+    this.service.save(this.form.value)
+    .subscribe(result => console.log(result), error => this.onError());
+
   }
   onCancel() {
     console.log("Cancel")
+  }
+
+  private onError() {
+    this.snackBar.open('Erro ao Salvar Curso!', '', { duration: 5000 });
   }
 
 }
