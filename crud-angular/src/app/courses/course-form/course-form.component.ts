6ff5bae868e0e25eb8cd6ee76a6bef98330760b5
Mateus H M Ferreira <mateushenrique123@live.com>
2025-05-07 17:06:24 -0300

    Ajuste do componente para enviar os dados

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CoursesService } from '../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
@@ -14,7 +15,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -27,11 +29,16 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value)
-    .subscribe(result => console.log(result), error => this.onError());
+    .subscribe(result => this.onSuccess(), error => this.onError());
 
   }
   onCancel() {
-    console.log("Cancel")
+    this.location.back();
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Cusro salvo com sucesso!', '', { duration: 5000 });
+    this.location.back();
   }
 
   private onError() {
