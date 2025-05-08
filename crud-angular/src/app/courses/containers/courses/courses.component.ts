99eaadfb48260af2d1fa61ba3cda816db630dd10
Mateus H M Ferreira <mateushenrique123@live.com>
2025-05-08 18:06:58 -0300

    Refatoração e ajuste de diretórios

--- crud-angular/src/app/courses/containers/courses/courses.component.ts
+++ crud-angular/src/app/courses/containers/courses/courses.component.ts
@@ -1,6 +1,6 @@
-import { CoursesService } from './../services/courses.service';
+import { CoursesService } from '../../services/courses.service';
 import { Component, OnInit } from '@angular/core';
-import { Course } from '../model/course';
+import { Course } from '../../model/course';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
@@ -42,7 +42,7 @@ export class CoursesComponent implements OnInit {
       data: errorMassage
     });
   }
-  
+
 
   ngOnInit(): void {
   }
